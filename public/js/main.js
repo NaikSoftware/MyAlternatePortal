36b8f971c4a69274caee40acfa1310fea256ace0
Naik <naiksav4enko@gmail.com>
2015-09-09 21:10:17 +0300

    Create upload form

--- public/js/main.js
+++ public/js/main.js
@@ -58,6 +58,29 @@ $(function () {
         }
     }
 
+    /* Setup file selecting fields */
+    $(document).on('change', '.btn-file :file', function () {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    });
+
+    $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        console.log(input, log);
+
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log) alert(log);
+        }
+
+    });
+
 });
 
 
