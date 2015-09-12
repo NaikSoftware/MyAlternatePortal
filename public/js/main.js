569e7f822108c34e65b8ef32bd9e05d5e1222eaf
Naik <naiksav4enko@gmail.com>
2015-09-12 10:39:33 +0300

    Removed set/unset navbar fixed on different screens

--- public/js/main.js
+++ public/js/main.js
@@ -29,23 +29,6 @@ $(function () {
         window.location.href = '/logout';
     }
 
-
-    /* Adjust screen size */
-    var navbar = $('#navbar');
-    var body = $('body');
-
-    $(window).resize(adjustScreenSize($(window).width()));
-
-    function adjustScreenSize(width) {
-        if (width < 800) {
-            navbar.removeClass('navbar-fixed-top');
-            body.css('padding-top', '0px');
-        } else {
-            navbar.addClass('navbar-fixed-top');
-            body.css('padding-top', '70px');
-        }
-    }
-
     /* Setup file selecting fields */
     $(document).on('change', '.btn-file :file', function () {
         var input = $(this),
