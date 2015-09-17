cc7dfc62986e8962f9bf2746048b183ef374d767
Naik <naiksav4enko@gmail.com>
2015-09-17 20:56:25 +0300

    Saving schedule infrastructure

--- models.js
+++ models.js
@@ -10,8 +10,17 @@ module.exports = function Models(mongoose) {
     this.User = mongoose.model('users', userSchema);
 
     var facultySchema = new Schema({name: String});
-    var courseSchema = new Schema({facultyId: String, name: String});
-    var groupSchema = new Schema({name: String, facultyId: String, courseId: String});
+
+    var courseSchema = new Schema({
+        name: String,
+        facultyId: Schema.Types.ObjectId
+    });
+
+    var groupSchema = new Schema({
+        name: String,
+        facultyId: Schema.Types.ObjectId,
+        courseId: Schema.Types.ObjectId
+    });
 
     this.Faculty = mongoose.model('faculties', facultySchema);
     this.Course = mongoose.model('courses', courseSchema);
@@ -19,6 +28,7 @@ module.exports = function Models(mongoose) {
 
     // Schedule on 1 week
     var scheduleSchema = new Schema({
+        groupId: Schema.Types.ObjectId,
         startTime: Date,
         days: [{
             name: String,
--- routes/SaveSchedule.js
+++ routes/SaveSchedule.js
@@ -23,20 +23,65 @@ module.exports = function SaveSchedule(models) {
             return;
         }
 
-        try {
-            var weeks = converter(req.file);
-            weeks.forEach(function (week) {
-                console.log(JSON.stringify(week, null, 4));
+        if (!checkQuery(req.body)) {
+            res.status(400).end();
+            return;
+        }
+
+        var faculty, course, group;
+        if (req.body.faculty.type === 'new') {
+            faculty = new models.Faculty({name: req.body.faculty.val});
+            faculty.save();
+        }
+        if (req.body.course.type === 'new' && faculty) {
+            course = new models.Course({
+                name: req.body.course.val,
+                facultyId: faculty.id
             });
-            res.send('{}');
-        } catch(e) {
-            console.log(e);
-            res.status(422).send(e.message);
+            course.save();
+        }
+        if (req.body.group.type === 'new' && course) {
+            group = new models.Group({
+                name: req.body.group.val,
+                facultyId: faculty.id,
+                courseId: course.id
+            });
+            group.save();
+            convert(group);
+        } else {
+            models.Group.findById(req.body.group.val, function (err, gr) {
+                convert(gr);
+            });
         }
     });
 
+    function convert(group) {
+        try {
+            var weeks = converter(req.file);
+            weeks.forEach(function (week) {
+                //week.groupId =
+                //new models.Schedule(week).save();
+            });
+            res.send('{}');
+        } catch (e) {
+            console.log(e);
+            res.status(422).send(e.message);
+        }
+    }
+
     function checkResult(err, result) {
         return !err && result && result.length > 0;
+    }
+
+    function checkQuery(body) {
+        return req.file
+            && checkVar(body.faculty)
+            && checkVar(body.course)
+            && checkVar(body.group);
+    }
+
+    function checkVar(v) {
+        return v && v.val;
     }
 
 };
